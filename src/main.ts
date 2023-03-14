da09e407c21fd50d3097fd38ee0c5a30a077f0d4
plinio8899 <plinio8899@gmail.com>
2023-03-13 20:33:54 -0500

    Se crea el resource o crud llamado item

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('My api rest')
+    .setDescription('La documentacion de mi api')
+    .setVersion('1.0')
+    .addTag('items')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
   await app.listen(3000);
 }
 bootstrap();
